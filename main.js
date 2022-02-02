8eff25c670140f08d89d074d7b12b378fa3aeb1f
qshvera <oleksasinavera@gmail.com>
2022-02-02 18:28:33 +0200

    comit

---
+++ main.js
@@ -0,0 +1,102 @@
+
+
+// userName = 112345
+// userName = 112.123
+
+// console.log(userName)
+
+
+// let a = 1
+// let b = 0
+// let c = a/b
+// let v ='x'
+// let w = 1/v
+
+// console.log(c)
+// console.log(55/'jfhv')
+
+// let n = 1
+// console.log(n)
+
+// let m = 'hello'
+// let e = 'word'
+
+// console.log(m+e)
+
+
+
+// let a = 3
+// let b = 8
+// let c = '&'
+// console.log(`сумма= ${a+b}`,c)
+
+// let isSmoking = false
+// let f = 4>1;
+// console.log(f)
+// console.log(isSmoking)
+
+
+
+// let a = null
+// console.log(a)
+// console.log(typeof(a))
+
+
+
+
+
+// let a = 0
+// console.log(typeof(a))
+// //number
+
+// let b = 10n
+// console.log(typeof(b))
+// //bigint
+
+// let c = true
+// console.log(typeof(c))
+// //boolean
+
+// let d = "foo"
+// console.log(typeof(d))
+// //string
+
+// let e = 'Symbol'
+// console.log(typeof(e))
+// //string
+
+// let m = Math
+// console.log(typeof(m))
+// //object
+
+// let n = null
+// console.log(typeof(n))
+// //object
+
+// let f = alert
+// console.log(typeof(f))
+// //function
+
+// let a = prompt ('ведите переменую?' )
+// console.log(a)
+// console.log(typeof(a))
+
+// let a = Number (prompt("первое число?"))
+// let b = Number (prompt("второе число?"))
+// alert(a + b)
+
+// let a =  +prompt("первое число?")
+// let b = +prompt("второе число?")
+// alert(a + b)
+
+// let name ="Ilya"
+// alert(`hello  ${1}`)
+// alert(`hello ${"name"}`)
+// alert(`hello ${name}`)
+// alert(`hello ${1+3+5}`)
+
+task.onclick = function () {
+    console.log('Hello');
+    alert ('Hello, World (da)')
+    let name = prompt('Кто ты?')
+    console.log('my name is ' + name) }
